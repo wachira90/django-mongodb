ca343458a5729a6eeff0d093cca37d1c6b8d148d
wachira90 <wcrcorp@gmail.com>
2022-11-04 18:40:43 +0700

    2022-11-04 18:40:43.40

--- appmain/api/admin.py
+++ appmain/api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Musician, Album
 
 # Register your models here.
+class AlbumDisp(admin.ModelAdmin):
+    list_display = ('artist', 'name', 'release_date', 'num_stars')
+
+admin.site.register(Album, AlbumDisp)
+
+class MusicianDisp(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'instrument')
+
+admin.site.register(Musician, MusicianDisp)
